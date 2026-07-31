86991cfcf5aa0fef4d3184e1342de39de64e8169
DmitryTarasov <user_274@mail.ru>
2026-08-01 01:36:38 +0300

    Upgrade settings.py and deploy.sh files

--- deploy.sh
+++ deploy.sh
@@ -21,3 +21,20 @@ sudo systemctl restart nginx
 sudo systemctl restart $SERVICE_NAME
 
 echo "Деплой завершен успешно"
+
+set -a
+source .env
+set +a
+
+curl -s -H "X-Rollbar-Access_Token: $ROLLBAR_ACCESS_TOKEN" \
+     -H "Content-Type: application/json" \
+     -X POST "https://api.rollbar.com/api/1/deploy" \
+     -d '{
+          "environment": "'"$ROLLBAR_ENVIRONMENT"'",
+          "revision": "'"$(git rev-parse HEAD)"'",
+          "local_username": "'"$(whoami)"'",
+          "comment": "Deploy via script from '"$(hostname)"'",
+          "status": "succeeded"
+         }'
+
+echo "Уведомление в Rollbar отправлено"
